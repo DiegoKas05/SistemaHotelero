8a146ec62372e87ab5e21b4296d39eac80717e59
Jonatha Fuentes <jonathan.fuentes@catolica.edu.sv>
2025-05-18 09:24:45 -0600

    estos son unos cambios que he hecho para recepcion.

--- SistemaTienda.AccesoDatos/Data/Repository/ApplicationUserRepository.cs
+++ SistemaTienda.AccesoDatos/Data/Repository/ApplicationUserRepository.cs
@@ -1,14 +1,20 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using SistemaHotelero.Data;
 using SistemaHotelero.DataAccess.Data.Repository.iRepository;
 using SistemaHotelero.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 public class ApplicationUserRepository : IApplicationUserRepository
 {
     private readonly UserManager<ApplicationUser> _userManager;
     private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly ApplicationDbContext _db;
 
-    public ApplicationUserRepository(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+    public ApplicationUserRepository(ApplicationDbContext db, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
     {
+        _db = db;
         _userManager = userManager;
         _roleManager = roleManager;
     }
@@ -19,16 +25,22 @@ public class ApplicationUserRepository : IApplicationUserRepository
 
         if (resultado.Succeeded)
         {
-            // Asegura que el rol exista
             if (!await _roleManager.RoleExistsAsync(rolUsuario))
             {
                 await _roleManager.CreateAsync(new IdentityRole(rolUsuario));
             }
 
-            // Asignar rol al usuario
             await _userManager.AddToRoleAsync(usuario, rolUsuario);
         }
 
         return resultado;
     }
+
+    // esto es nuevo lo coloque yo para lo de agregar un cliente con una habitacion
+    public IEnumerable<ApplicationUser> GetAll()
+    {
+        return _userManager.Users.ToList();
+
+    }
 }
+
